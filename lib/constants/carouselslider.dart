16d72b708f88a314cbc17b177a45f1c348aeae45
Reyhane Fallah <reyhanefallah2002@gmail.com>
2023-03-22 01:57:30 +0430

    clean code carosel

--- lib/constants/carouselslider.dart
+++ lib/constants/carouselslider.dart
@@ -1,56 +1,8 @@
-// import 'dart:convert';
-
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:front_esignco/network/slidermodel.dart';
-// import 'package:front_esignco/network/slidermodel.dart';
-
-// class ImageSlider extends StatelessWidget {
-//   const ImageSlider({super.key, required this.photos});
-
-//   //Carousel ImageSlider = Carousel(id: '', image: 'images/1.png');
-//   final List<Photo> photos;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: MediaQuery.of(context).size.width,
-//       child: CarouselSlider.builder(
-//         options: CarouselOptions(
-//           disableCenter: false,
-//           padEnds: false,
-//           height: 184.0,
-//           viewportFraction: 0.9,
-//           enableInfiniteScroll: false,
-//         ),
-//         itemCount: photos.length,
-//         itemBuilder: (context, index, realIndex) {
-//           final firstImage = photos[index];
-
-//           return buildImage(photos as String, index);
-//         },
-//       ),
-//     );
-//   }
-// }
-
-// Widget buildImage(String Images, int index) {
-//   return ClipRRect(
-//     borderRadius: BorderRadius.circular(20.0),
-//     child: Container(
-//       margin: EdgeInsets.symmetric(horizontal: 4.0),
-//       child: Image.asset(
-//         Images,
-//         fit: BoxFit.cover,
-//       ),
-//     ),
-//   );
-// }
-
-// import 'package:carousel_slider/carousel_slider.dart';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import '../network/slidermodel.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class ImageSlider extends StatelessWidget {
   const ImageSlider({Key? key}) : super(key: key);
@@ -67,6 +19,7 @@ class ImageSlider extends StatelessWidget {
           );
         } else if (snapshot.hasData) {
           return Container(
+            margin: EdgeInsets.symmetric(horizontal: 12.0),
             width: MediaQuery.of(context).size.width,
             child: CarouselSlider.builder(
               options: CarouselOptions(
@@ -78,8 +31,13 @@ class ImageSlider extends StatelessWidget {
               ),
               itemCount: data!['data']['result'].length,
               itemBuilder: (context, index, realIndex) {
-                return Image.network(
-                    '${data['data']['result'][index]['imageSmall']}');
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Image.network(
+                    '${data['data']['result'][index]['imageSmall']}',
+                    fit: BoxFit.fill,
+                  ),
+                );
               },
             ),
           );
@@ -93,38 +51,9 @@ class ImageSlider extends StatelessWidget {
   }
 }
 
-
-// class PhotosList extends StatelessWidget {
-//   const PhotosList({super.key});
-
-  // final List<Photo> photos;
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Container(
-  //     width: MediaQuery.of(context).size.width,
-  //     child: CarouselSlider.builder(
-  //       options: CarouselOptions(
-  //         disableCenter: false,
-  //         padEnds: false,
-  //         height: 184.0,
-  //         viewportFraction: 0.9,
-  //         enableInfiniteScroll: false,
-  //       ),
-  //       itemCount: data.length,
-  //       itemBuilder: (context, index, realIndex) {
-  //         return Image.network(photos[index].imageLarge);
-  //       },
-  //     ),
-  //   );
-    // GridView.builder(
-    //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-    //     crossAxisCount: 2,
-    //   ),
-    //   itemCount: photos.length,
-    //   itemBuilder: (context, index) {
-    //     return Image.network(photos[index].thumbnailUrl);
-    //   },
-    // );
-//   }
-// }
+Future<Map> getPics() async {
+  String url =
+      'https://ws.esignco.ir/api/v1/slider/banner?SliderType=slider&LandingPageId=home-landing';
+  http.Response response = await http.get(Uri.parse(url));
+  return json.decode(response.body);
+}
